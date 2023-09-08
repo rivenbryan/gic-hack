49abc1657b26612fab7614df1b88957a1a0c01a1
tayb <bryan.tay@autodesk.com>
2023-09-08 23:30:16 +0800

    init for database set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-import Navbar from '@/components/Navbar'
-import { Typography } from '@mui/material'
-import Image from 'next/image'
+"use client"
+import Navbar from "@/components/Navbar";
+import { supabase } from "@/lib/db";
+import { Button, Container, Typography } from "@mui/material";
+import Image from "next/image";
+import React, { useEffect } from "react";
 
 export default function Home() {
+
   return (
     <main>
-      <Navbar/>
+      <Navbar />
     </main>
-  )
+  );
 }
